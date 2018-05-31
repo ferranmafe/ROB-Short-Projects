215a6031eaf24c87420a184fcc1be30e511797d4
Daniel Martinez Bordes <dmartinezbordes@gmail.com>
2018-05-31 13:23:54 +0200

    Line following

--- P3/advanced_features/ObstacleAvoidance_Line_Following.py
+++ P3/advanced_features/ObstacleAvoidance_Line_Following.py
@@ -3,7 +3,7 @@
 
 import os
 import sys, tty, termios
-from math import sin, cos, fmod, atan2, sqrt, pow
+from math import sin, cos, fmod, atan2, sqrt, pow, fabs
 
 import time
 import serial
@@ -127,15 +127,120 @@ def informationToArray (info):
         output.append(elem.split(','))
     return output
 
+def updateOdometry():
+    global L_ini, R_ini
+    L, R = get_motors()
+    # Calculamos la odometria (con incertidumbre)
+    odometry(L, R)
+    # Actualizamos los valores L_ini, R_ini con L y R para poder calcular el siguiente incremento
+    L_ini, R_ini = L, R
+
+def followObstacleLeft():
+    global y_w, y_w_th, maxWallSpeed, maxWallDist, thLateralDistWall, thCentralDistWall, thRangeWall
+
+    avoided = False
+
+    leftMotorDist = maxWallDist
+    rightMotorDist = maxWallDist
+
+    sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
+    leftDist = getMedianDist(sensorInformation,  40, 90)
+    centralDist = getMedianDist(sensorInformation, 340, 20)
+
+    while not ((-1 < leftDist < thLateralDistWall) or (-1 < centralDist < thCentralDistWall)):
+        updateOdometry()
+
+        sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
+        leftDist = getMedianDist(sensorInformation,  40, 90)
+        centralDist = getMedianDist(sensorInformation, 340, 20)
+
+        envia(ser, 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxWallSpeed), 0.05)
+    while not avoided or fabs(y_w) > y_w_th:
+        updateOdometry()
+
+        if (fabs(y_w) > y_w_th):
+            avoided = True
+
+        leftPercentatge = 1
+        rightPercentatge = 1
+
+        sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
+        leftDist = getMedianDist(sensorInformation,  40, 90)
+        centralDist = getMedianDist(sensorInformation, 340, 20)
+
+        if -1 < centralDist < thCentralDistWall:
+            rightPercentatge = (thCentralDistWall - centralDist)/ thCentralDistWall
+            leftPercentatge = 1
+
+        elif not (-1 < leftDist < thLateralDistWall + thRangeWall):
+            rightPercentatge = 1
+            if (leftDist == -1):
+                leftPercentatge = 0.5
+            else:
+                leftPercentatge = thLateralDistWall / thRangeWall
+
+        elif -1 < leftDist < thLateralDistWall - thRangeWall:
+            rightPercentatge = leftDist / thLateralDistWall
+            leftPercentatge = 1
+
+        rightMotorDist = maxWallDist * rightPercentatge
+        leftMotorDist = maxWallDist * leftPercentatge
+        envia(ser, 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxWallSpeed), 0.05)
+
+def followObstacleRight():
+    global y_w, y_w_th, maxWallSpeed, maxWallDist, thLateralDistWall, thCentralDistWall, thRangeWall
+
+    avoided = False
+
+    leftMotorDist = maxWallDist
+    rightMotorDist = maxWallDist
+
+    sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
+    rightDist = getMedianDist(sensorInformation,  270, 320)
+    centralDist = getMedianDist(sensorInformation, 340, 20)
+
+    while not ((-1 < rightDist < thLateralDistWall) or (-1 < centralDist < thCentralDistWall)):
+        updateOdometry()
+
+        sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
+        rightDist = getMedianDist(sensorInformation, 270, 320)
+        centralDist = getMedianDist(sensorInformation, 340, 20)
+
+        envia(ser, 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxWallSpeed), 0.05)
+    while not avoided or fabs(y_w) > y_w_th:
+        updateOdometry()
+
+        if (fabs(y_w) > y_w_th):
+            avoided = True
+
+        leftPercentatge = 1
+        rightPercentatge = 1
+
+        sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
+        rightDist = getMedianDist(sensorInformation,  270, 320)
+        centralDist = getMedianDist(sensorInformation, 340, 20)
+
+        if -1 < centralDist < thCentralDistWall:
+            leftPercentatge = (thCentralDistWall - centralDist)/ thCentralDistWall
+            rightPercentatge = 1
+
+        elif not (-1 < rightDist < thLateralDistWall + thRangeWall):
+            leftPercentatge = 1
+            if (rightDist == -1):
+                rightPercentatge = 0.5
+            else:
+                rightPercentatge = thLateralDistWall / thRangeWall
+
+        elif -1 < rightDist < thLateralDistWall - thRangeWall:
+            leftPercentatge = rightDist / thLateralDistWall
+            rightPercentatge = 1
+
+        rightMotorDist = maxWallDist * rightPercentatge
+        leftMotorDist = maxWallDist * leftPercentatge
+        envia(ser, 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxWallSpeed), 0.05)
+
 
 if __name__ == '__main__':
-    global ser
-    # Open the Serial Port.
-    ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=0.1)
-    envia(ser,'TestMode On', 0.2)
-    envia(ser,'PlaySound 1', 0.3)
-    envia(ser, 'SetLDSRotation On', 0.4)
-
     global ser
     ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=0.05)
     envia(ser, 'TestMode On')
@@ -143,6 +248,7 @@ if __name__ == '__main__':
     envia(ser ,'SetMotor RWheelEnable LWheelEnable')
     envia(ser, 'SetLDSRotation On')
 
+    # Odometry params
     S = 121.5 # en mm
     V = [[0.01 ** 2, 0.],[0., 0.001 ** 2]]
     Pk0 = [[0.0001, 0.0001, 0.0001],[0.0001, 0.0001, 0.0001],[0.0001, 0.0001, 0.0001]]
@@ -155,18 +261,30 @@ if __name__ == '__main__':
     pose_t = transpose([[x_w, y_w, sum_theta]])
     Pk1 = Pk0
 
-    maxSpeed = 250
-    maxDist = 350
-    latDist = 100
-    thresholdDistance = 500
-    midThresholdDistance = 700
+    # ObstacleAvoidance params
+    maxObsSpeed = 250
+    maxObsCentralDist = 350
+    maxObsLateralDist = 100
+    thLateralDistObs = 500
+    thCentralDistObs = 700
+
+    # WallFollowing params
+    maxWallSpeed = 200
+    maxWallDist = 500
+    thLateralDistWall = 500
+    thCentralDistWall = 900
+    thRangeWall = 30
+
+    # Repositioning params
+    y_w_th = 50 # 5 centimetros de margen
+    sum_theta_th = 3 * 3.1415 / 180 # Threshold de 3 grados
 
     #Obtenemos distancia inicial de cada rueda
     L_ini, R_ini = get_motors()
-
-    leftMotorDist = maxDist
-    rightMotorDist = maxDist
+    leftMotorDist = maxObsCentralDist
+    rightMotorDist = maxObsCentralDist
     try:
+        # Mientras el laser no este funcionando mantenemos el robot parado
         init = 1
         while init:
             sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
@@ -176,42 +294,67 @@ if __name__ == '__main__':
             if  central != -1 or left != -1 or right != -1:
                 init = 0
                 envia(ser,'SetMotor RWheelEnable LWheelEnable', 0.2)
-                str(left) + " " + str(central) + " " + str(right) + "\n"
 
+        # Comportamiento ideal hay 2 casoss:
+        # - Ir recto como obstacleAvoidance, hasta que detectemos un objeto entonces girar
+        #   hacia el lado correspondiente y entrar en modo WallFollowing hasta que la distancia
+        #   de la componente y_w se recupere a 0.
+        # - Si el angulo orientado es mayor que un threshold respecto al angulo inicial (zero grados)
+        #   hay que reorientarse
         while 1:
-            L, R = get_motors()
-            # Calculamos la odometria (con incertidumbre)
-            odometry(L, R)
-            # Actualizamos los valores L_ini, R_ini con L y R para poder calcular el siguiente incremento
-            L_ini, R_ini = L, R
-            
-            sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
-            left = getMedianDist(sensorInformation, 20, 60)
-            central = getMedianDist(sensorInformation, 340, 20)
-            right = getMedianDist(sensorInformation, 300, 340)
+            # Siempre acutalizamos la odometria
+            updateOdometry()
+            # Comportamiento de obstacleAvoidance
+            if sum_theta < sum_theta_th or sum_theta > (2 * 3.1415 - sum_theta_th):
+                sensorInformation = informationToArray(envia(ser, 'GetLDSScan', 0.05))
+                left = getMedianDist(sensorInformation, 20, 60)
+                central = getMedianDist(sensorInformation, 340, 20)
+                right = getMedianDist(sensorInformation, 300, 340)
 
-            print str(left) + " " + str(central) + " " + str(right) + "\n"
+                if central != -1: k1 = min(1, central/thCentralDistObs)
+                else: k1 = 1
 
-            if central != -1: k1 = min(1, central/midThresholdDistance)
-            else: k1 = 1
+                k2 = left / thLateralDistObs
+                if not 0 <= k2 <= 1: k2 = 0
 
-            k2 = left / thresholdDistance
-            if not 0 <= k2 <= 1: k2 = 0
+                k3 = right / thLateralDistObs
+                if not 0 <= k3 <= 1: k3 = 0
 
-            k3 = right / thresholdDistance
-            if not 0 <= k3 <= 1: k3 = 0
+                if k2 == 0 and k3 == 0 and k1 != 1:
+                    if left > right:
+                        k3 = 1 - (central / thCentralDistObs)
+                    else:
+                        k2 = 1 - (central / thCentralDistObs)
 
-            if k2 == 0 and k3 == 0 and k1 != 1:
-                if left > right:
-                    k3 = 1 - (central / midThresholdDistance)
+                if k2 != 0:
+                    leftMotorDist = maxObsCentralDist * k1 + maxObsLateralDist * k2 - maxObsLateralDist * k3
+                    rightMotorDist = maxObsCentralDist * k1 - maxObsLateralDist * k2 + maxObsLateralDist * k3
+                    comando = 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxObsSpeed)
+                    envia(ser, comando, 0.05)
+                    followObstacleRight()
+                elif k3 != 0:
+                    leftMotorDist = maxObsCentralDist * k1 + maxObsLateralDist * k2 - maxObsLateralDist * k3
+                    rightMotorDist = maxObsCentralDist * k1 - maxObsLateralDist * k2 + maxObsLateralDist * k3
+                    comando = 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxObsSpeed)
+                    envia(ser, comando, 0.05)
+
+                    followObstacleLeft()
                 else:
-                    k2 = 1 - (central / midThresholdDistance)
+                    leftMotorDist = maxObsCentralDist * k1 + maxObsLateralDist * k2 - maxObsLateralDist * k3
+                    rightMotorDist = maxObsCentralDist * k1 - maxObsLateralDist * k2 + maxObsLateralDist * k3
+                    comando = 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxObsSpeed)
+                    envia(ser, comando, 0.05)
 
-            leftMotorDist = maxDist * k1 + latDist * k2 - latDist * k3
-            rightMotorDist = maxDist * k1 - latDist * k2 + latDist * k3
-
-            comando = 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxSpeed)
-            envia(ser, comando, 0.05)
+            # Nos reorientamos
+            else:
+                if sum_theta < 3.1415:
+                    k = sum_theta / 3.1415
+                else:
+                    k = -(2 * 3.1415 - sum_theta)/3.1415
+                rightMotorDist = maxObsCentralDist * k
+                leftMotorDist = -maxObsCentralDist * k
+                comando = 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxObsSpeed)
+                envia(ser, comando, 0.05)
 
     except KeyboardInterrupt:
         envia(ser, 'SetLDSRotation Off', 0.2)
