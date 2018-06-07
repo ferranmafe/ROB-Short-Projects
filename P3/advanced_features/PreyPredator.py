139f583c843dd709ed8d7c49e4646cb2a3265039
Ferran Martínez <ferran.mafe@gmail.com>
2018-06-07 11:58:39 +0200

    Add files via upload

--- P3/advanced_features/PreyPredator.py
+++ P3/advanced_features/PreyPredator.py
@@ -21,7 +21,16 @@ def envia(ser, missatge,temps=0.1, show_time = False):
         t = time.time() - first_time
         print("Round time: ",t)
     return rbuffer
-
+    
+def median(lst):
+    n = len(lst)
+    if n < 1:
+            return None
+    if n % 2 == 1:
+            return sorted(lst)[n//2]
+    else:
+            return sum(sorted(lst)[n//2-1:n//2+1])/2.0
+            
 # Funcion que encuentra el objeto más cercano al neato, descartando muros
 def getMinDist(info):
     global robotFrontDist, groupSize, numSimSens, wallTh
@@ -37,21 +46,30 @@ def getMinDist(info):
     # caso para un sensor todos los valores fueran None, el sensor tendría como valor None.
     # Nota: el indice del vector de sensores indica a que angulo hace referencia el valor almacenado en ese indice,
     # siendo el angulo del cada sensor su índice en el vector multiplicado por groupSize
+        
     sensors = []
     for i in range(len(aux)/groupSize):
         values = []
         for j in range(groupSize):
-            mod = (i + j) % 360
+            mod = ((i * groupSize) + j) % 360
             if aux[mod] is not None:
                 values.append(aux[mod])
-        if (len(values) > 0): sensors.append(float(sum(values))/len(values))
+        if (len(values) > 0): 
+            print values
+            print float(sum(values)) / max(len(values), 1)
+            sensors.append(float(sum(values)) / max(len(values), 1))
         else: sensors.append(None)
-
+    
+    for i in range(len(sensors)):
+        if sensors[i] is None:
+            print str(i) + " None"
+        else:
+            print str(i) + " " + str(sensors[i])
     # Una vez tenemos los sensores discretizados recorremos el vector de sensores para buscar muros.
     # Para cada sensor, recorremos tantos sensores como indique la variable (numSimSens), y si todos los
     # sensores por los que pasamos son similares (No son None y su diferencia es menor al threshold) consideramos
     # que están leyendo muro y lo guardamos en la variable wallSet
-    wallSet = {}
+    wallSet = set([])
     sensorsChecked = [False] * len(sensors)
     while not all(item for item in sensorsChecked):
         i = getNextNotCheckedIndex(sensorsChecked)
@@ -80,6 +98,7 @@ def getMinDist(info):
     for i in range(len(sensors)):
         if i not in wallSet and sensors[i] is not None:
             final_sensors.append((i * groupSize, sensors[i]))
+    
 
     # Finalmente, obtenemos la lectura más pequeña de los sensores leidos. Retornamos la tupla
     # (angulo, distancia)
@@ -94,7 +113,7 @@ def getMinDist(info):
 
 # Funcion que dado un vector de booleanos, retorna el indice del elemento a False. Si no hay ninguno retorna -1
 def getNextNotCheckedIndex(array):
-    for i in range(array):
+    for i in range(len(array)):
         if not array[i]: return i
     return -1
 
@@ -106,6 +125,21 @@ def informationToArray (info):
         output.append(elem.split(','))
     return output
 
+# Obtenemos la distancia minima como la media de los 3 valores minimos en el rango establecido
+def getMinDistWithoutAvoidingWalls(info):
+    global robotFrontDist
+    aux = []
+    for i in range(2, 362):
+        if 0 < int(info[i][1]) < 16627: aux.append((int(info[i][0]), (int(info[i][1]))))
+
+    if len(aux) > 0:
+        minim = aux[0]
+        for i in range(1, len(aux)):
+            if minim[1] > aux[i][1]: minim = aux[i]
+
+        return minim
+    # Si todos sus sensores detectan -1 el robot puede tener el laser apagado lo hacemos pararse
+    else: return (0, robotFrontDist)
 
 if __name__ == '__main__':
     global ser
@@ -119,22 +153,25 @@ if __name__ == '__main__':
     envia(ser, 'SetLDSRotation On', 0.3)
 
     # Velocidad máxima del robot
-    maxSpeed = 200
+    maxSpeed = 300
     # Distáncia máxima a recorrer en una solo comando para moverse al Neato
     maxDist = 400
 
     # Constante que limita cuan cerca está el robot del obstaculo
     robotFrontDist = 245
+    # Distancia a la que el robot empieza a huir
+    robotAvoidDist = 1000
     # Numero de angulos a discretizar como un sensor
     groupSize = 10
     # Numero de sensores con valores similares para ser considerados muro
-    numSimSens = 3
+    numSimSens = 5
     # Threshold para marcar cuan diferentes han de ser dos lecturas consecutivas para ser considerado que no es un muro
-    wallTh = 800
+    wallTh = 500
 
     # Inicializamos cada motor a una distáncia de maxDist
     leftMotorDist = maxDist
     rightMotorDist = maxDist
+    frontDist = 400
 
     # Pedimos al usuario que rol quiere desempeñar
     playerType = raw_input('Please, type if you want to be "Prey" or "Predator":\n')
@@ -146,12 +183,13 @@ if __name__ == '__main__':
         while 1:
             # Recogemos los datos del laser y obtenemos la minima distáncia
             data = informationToArray(envia(ser, 'GetLDSScan', 0.05))
-            minDist = getMinDist(data)
-            print("Min Dist: {}".format(minDist))
 
             # Si el rol es depredador
             if playerType == "Predator":
+                minDist = getMinDist(data)
+                print("Min Dist: {}".format(minDist))
                 # Modificamos los valores de k para acercarnos a la distáncia mínima
+                
                 if (minDist[0] <= 180):
                     k = minDist[0]/180.
                 else:
@@ -163,21 +201,25 @@ if __name__ == '__main__':
 
                 # Enviamos el resultado al neato para que se mueva
                 envia(ser, 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxSpeed), 0.05)
-
-            # Si el rol es depredador
+                
+            # Si el rol es presa
             elif playerType == "Prey":
+                minDist = getMinDistWithoutAvoidingWalls(data)
+                print("Min Dist: {}".format(minDist))
                 # Modificamos los valores de k para alejarnos de la distáncia mínima
-                if (minDist[0] > 180):
-                    k = minDist[0] / 180.
+                if (minDist[0] <= 180):
+                    k = 1 - (minDist[0] / 180.)
                 else:
-                    k = -(360 - minDist[0]) / 180.
+                    k = -(1 - (360 - minDist[0]) / 180.)
+                
                 # Computamos las ecuaciones lineales para calcular cuanto se ha de mover cada rueda
-                rightMotorDist = maxDist * k + (minDist[1] - robotFrontDist)
-                leftMotorDist = -maxDist * k + (minDist[1] - robotFrontDist)
-                print("leftMotorDist: {} rightMotorDist: {}".format(rightMotorDist, leftMotorDist))
+                if minDist[0] < robotAvoidDist:
+                    rightMotorDist = -maxDist * k + frontDist
+                    leftMotorDist = maxDist * k + frontDist
+                    print("leftMotorDist: {} rightMotorDist: {}".format(rightMotorDist, leftMotorDist))
 
-                # Enviamos el resultado al neato para que se mueva
-                envia(ser, 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxSpeed), 0.05)
+                    # Enviamos el resultado al neato para que se mueva
+                    envia(ser, 'SetMotor LWheelDist ' + str(leftMotorDist) + ' RWheelDist ' + str(rightMotorDist) + ' Speed ' + str(maxSpeed), 0.05)
 
     except KeyboardInterrupt:
         envia(ser, 'SetLDSRotation Off', 0.2)
